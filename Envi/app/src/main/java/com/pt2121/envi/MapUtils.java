cae6cc066f10fec6987a183232db2162861a5416
Prat <banqfirn@gmail.com>
2015-01-20 09:36:31 -0500

    Add null check and clean up

--- Envi/app/src/main/java/com/pt2121/envi/MapUtils.java
+++ Envi/app/src/main/java/com/pt2121/envi/MapUtils.java
@@ -32,20 +32,16 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 import com.pt2121.envi.model.Loc;
 
-import android.content.Context;
 import android.location.Location;
 import android.util.Log;
 import android.util.Pair;
-
-import java.util.List;
 
 import rx.Observable;
 import rx.Subscriber;
 import rx.Subscription;
 import rx.android.schedulers.AndroidSchedulers;
-import rx.functions.Action1;
-import rx.functions.Func1;
 import rx.schedulers.Schedulers;
+import rx.subscriptions.Subscriptions;
 
 /**
  * Created by prt2121 on 1/19/15.
@@ -54,71 +50,22 @@ public class MapUtils {
 
     private static final String TAG = MapUtils.class.getSimpleName();
 
-    public static Func1<Location, Observable<List<Loc>>> getFindClosestBins(
-            Context context, final long latitude, final long longitude) {
-        return location -> RecycleApp.getRecycleMachine(context)
-                .finBin().getLocs()
-                .toSortedList(LocUtils.compare(latitude, longitude));
-    }
-
     /**
      * Show the pins on the map.
      *
-     * @param userLocationObservable an observable of user's location
-     * @param findClosest            a function that returns an observable of a sorted list of
-     *                               {@code Loc}
-     * @param maxLocation            a maximum number of pins
-     */
-    public static Subscription showPins(Observable<Location> userLocationObservable,
-            Func1<Location, Observable<List<Loc>>> findClosest,
-            GoogleMap map,
-            int maxLocation) {
-        return userLocationObservable
-                .flatMap(findClosest)
-                .flatMap(Observable::from)
-                .take(maxLocation).onBackpressureBuffer()
-                .subscribeOn(Schedulers.newThread())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Subscriber<Loc>() {
-
-                    @Override
-                    public void onCompleted() {
-                        Log.d(TAG, "onCompleted");
-                    }
-
-                    @Override
-                    public void onError(Throwable e) {
-                        Log.e(TAG, e.getMessage());
-                    }
-
-                    @Override
-                    public void onNext(Loc loc) {
-                        if (map != null) {
-                            map.addMarker(new MarkerOptions()
-                                    .position(new LatLng(loc.latitude, loc.longitude))
-                                    .title(loc.name)
-                                    .icon(BitmapDescriptorFactory
-                                            .defaultMarker(175))); //HSL: 175° 100% 34%
-                        }
-                    }
-                });
-    }
-
-    /**
-     * Show the pins on the map.
-     *
-     * @param pivot user's location or the center location.
-     * @param things bin locations or things
-     * @param map the map to be showed
+     * @param pivot       user's location or the center location.
+     * @param things      bin locations or things
+     * @param map         the map to be showed
      * @param maxLocation the number of things
-     * @param hue the hue of the pin
+     * @param hue         the hue of the pin
      * @return Subscription
      */
     public static Subscription showPins(Observable<Location> pivot, Observable<Loc> things,
             GoogleMap map, int maxLocation, int hue) {
-        if(map == null)
-            Log.d(TAG, "map is NULL");
-
+        if (map == null) {
+            Log.e(TAG, "map is NULL");
+            return Subscriptions.empty();
+        }
         return Observable.zip(pivot.repeat(), things,
                 (location, loc) -> {
                     Location l = new Location(loc.name);
