4b905f20917999dc3125f07dd4b442a1be157584
Prat <banqfirn@gmail.com>
2015-04-26 19:10:35 -0400

    Reformat code

--- scrollable-imageview/app/src/main/java/com/egorand/scrollableimageview/MainActivity.java
+++ scrollable-imageview/app/src/main/java/com/egorand/scrollableimageview/MainActivity.java
@@ -1,7 +1,5 @@
 package com.egorand.scrollableimageview;
 
-import java.io.IOException;
-
 import android.app.Activity;
 import android.content.res.AssetManager;
 import android.graphics.Bitmap;
@@ -14,56 +12,58 @@ import android.view.ViewGroup.LayoutParams;
 import android.view.Window;
 import android.widget.ImageView;
 
+import java.io.IOException;
+
 public class MainActivity extends Activity {
 
-	private static final String DEBUG_TAG = "Training";
+    private static final String DEBUG_TAG = "Training";
 
-	private ImageView imageView;
+    private ImageView imageView;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-		requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
+        requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
 
-		setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
 
-		new BitmapLoaderTask().execute("Santiago_Bernabeu_Stadium.jpg");
-	}
+        new BitmapLoaderTask().execute("Santiago_Bernabeu_Stadium.jpg");
+    }
 
-	private void setImageBitmap(Bitmap bmp) {
-		imageView = new ScrollableImageView(this);
-		imageView.setLayoutParams(new LayoutParams(bmp.getWidth(), bmp.getHeight()));
-		imageView.setImageBitmap(bmp);
-		ViewGroup root = (ViewGroup) findViewById(android.R.id.content);
-		root.addView(imageView);
-	}
+    private void setImageBitmap(Bitmap bmp) {
+        imageView = new ScrollableImageView(this);
+        imageView.setLayoutParams(new LayoutParams(bmp.getWidth(), bmp.getHeight()));
+        imageView.setImageBitmap(bmp);
+        ViewGroup root = (ViewGroup) findViewById(android.R.id.content);
+        root.addView(imageView);
+    }
 
-	private class BitmapLoaderTask extends AsyncTask<String, Void, Bitmap> {
+    private class BitmapLoaderTask extends AsyncTask<String, Void, Bitmap> {
 
-		@Override
-		protected void onPreExecute() {
-			super.onPreExecute();
-			setProgressBarIndeterminateVisibility(true);
-		}
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            setProgressBarIndeterminateVisibility(true);
+        }
 
-		@Override
-		protected Bitmap doInBackground(String... params) {
-			AssetManager assets = getAssets();
-			Bitmap bmp = null;
-			try {
-				bmp = BitmapFactory.decodeStream(assets.open(params[0]));
-			} catch (IOException e) {
-				Log.e(DEBUG_TAG, e.getMessage(), e);
-			}
-			return bmp;
-		}
+        @Override
+        protected Bitmap doInBackground(String... params) {
+            AssetManager assets = getAssets();
+            Bitmap bmp = null;
+            try {
+                bmp = BitmapFactory.decodeStream(assets.open(params[0]));
+            } catch (IOException e) {
+                Log.e(DEBUG_TAG, e.getMessage(), e);
+            }
+            return bmp;
+        }
 
-		@Override
-		protected void onPostExecute(Bitmap result) {
-			super.onPostExecute(result);
-			setProgressBarIndeterminateVisibility(false);
-			setImageBitmap(result);
-		}
-	}
+        @Override
+        protected void onPostExecute(Bitmap result) {
+            super.onPostExecute(result);
+            setProgressBarIndeterminateVisibility(false);
+            setImageBitmap(result);
+        }
+    }
 }
--- scrollable-imageview/app/src/main/java/com/egorand/scrollableimageview/ScrollableImageView.java
+++ scrollable-imageview/app/src/main/java/com/egorand/scrollableimageview/ScrollableImageView.java
@@ -13,108 +13,110 @@ import android.widget.OverScroller;
  * An extension to the standard Android {@link ImageView}, which makes it
  * respond to Scroll and Fling events. Uses a {@link GestureDetectorCompat} and
  * a {@link OverScroller} to provide scrolling functionality.
- * 
+ *
  * @author EgorAnd
- * 
  */
 public class ScrollableImageView extends ImageView {
 
-	private GestureDetectorCompat gestureDetector;
-	private OverScroller overScroller;
+    private GestureDetectorCompat gestureDetector;
 
-	private final int screenW;
-	private final int screenH;
+    private OverScroller overScroller;
 
-	private int positionX = 0;
-	private int positionY = 0;
+    private final int screenW;
 
-	public ScrollableImageView(Context context) {
-		super(context);
+    private final int screenH;
 
-		// We will need screen dimensions to make sure we don't overscroll the
-		// image
-		DisplayMetrics dm = getResources().getDisplayMetrics();
-		screenW = dm.widthPixels;
-		screenH = dm.heightPixels;
+    private int positionX = 0;
 
-		gestureDetector = new GestureDetectorCompat(context, gestureListener);
-		overScroller = new OverScroller(context);
-	}
+    private int positionY = 0;
 
-	@Override
-	public boolean onTouchEvent(MotionEvent event) {
-		gestureDetector.onTouchEvent(event);
-		return true;
-	}
+    public ScrollableImageView(Context context) {
+        super(context);
 
-	@Override
-	public void computeScroll() {
-		super.computeScroll();
-		// computeScrollOffset() returns true only when the scrolling isn't
-		// already finished
-		if (overScroller.computeScrollOffset()) {
-			positionX = overScroller.getCurrX();
-			positionY = overScroller.getCurrY();
-			scrollTo(positionX, positionY);
-		} else {
-			// when scrolling is over, we will want to "spring back" if the
-			// image is overscrolled
-			overScroller.springBack(positionX, positionY, 0,
-					getMaxHorizontal(), 0, getMaxVertical());
-		}
-	}
+        // We will need screen dimensions to make sure we don't overscroll the
+        // image
+        DisplayMetrics dm = getResources().getDisplayMetrics();
+        screenW = dm.widthPixels;
+        screenH = dm.heightPixels;
 
-	private int getMaxHorizontal() {
-		return (getDrawable().getBounds().width() - screenW);
-	}
+        gestureDetector = new GestureDetectorCompat(context, gestureListener);
+        overScroller = new OverScroller(context);
+    }
 
-	private int getMaxVertical() {
-		return (getDrawable().getBounds().height() - screenH);
-	}
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        gestureDetector.onTouchEvent(event);
+        return true;
+    }
 
-	private SimpleOnGestureListener gestureListener = new SimpleOnGestureListener() {
+    @Override
+    public void computeScroll() {
+        super.computeScroll();
+        // computeScrollOffset() returns true only when the scrolling isn't
+        // already finished
+        if (overScroller.computeScrollOffset()) {
+            positionX = overScroller.getCurrX();
+            positionY = overScroller.getCurrY();
+            scrollTo(positionX, positionY);
+        } else {
+            // when scrolling is over, we will want to "spring back" if the
+            // image is overscrolled
+            overScroller.springBack(positionX, positionY, 0,
+                    getMaxHorizontal(), 0, getMaxVertical());
+        }
+    }
 
-		@Override
-		public boolean onDown(MotionEvent e) {
-			overScroller.forceFinished(true);
-			ViewCompat.postInvalidateOnAnimation(ScrollableImageView.this);
-			return true;
-		}
+    private int getMaxHorizontal() {
+        return (getDrawable().getBounds().width() - screenW);
+    }
 
-		@Override
-		public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX,
-				float velocityY) {
-			overScroller.forceFinished(true);
-			overScroller.fling(positionX, positionY, (int) -velocityX,
-					(int) -velocityY, 0, getMaxHorizontal(), 0,
-					getMaxVertical());
-			ViewCompat.postInvalidateOnAnimation(ScrollableImageView.this);
-			return true;
-		}
+    private int getMaxVertical() {
+        return (getDrawable().getBounds().height() - screenH);
+    }
 
-		@Override
-		public boolean onScroll(MotionEvent e1, MotionEvent e2,
-				float distanceX, float distanceY) {
-			overScroller.forceFinished(true);
-			// normalize scrolling distances to not overscroll the image
-			int dx = (int) distanceX;
-			int dy = (int) distanceY;
-			int newPositionX = positionX + dx;
-			int newPositionY = positionY + dy;
-			if (newPositionX < 0) {
-				dx -= newPositionX;
-			} else if (newPositionX > getMaxHorizontal()) {
-				dx -= (newPositionX - getMaxHorizontal());
-			}
-			if (newPositionY < 0) {
-				dy -= newPositionY;
-			} else if (newPositionY > getMaxVertical()) {
-				dy -= (newPositionY - getMaxVertical());
-			}
-			overScroller.startScroll(positionX, positionY, dx, dy, 0);
-			ViewCompat.postInvalidateOnAnimation(ScrollableImageView.this);
-			return true;
-		}
-	};
+    private SimpleOnGestureListener gestureListener = new SimpleOnGestureListener() {
+
+        @Override
+        public boolean onDown(MotionEvent e) {
+            overScroller.forceFinished(true);
+            ViewCompat.postInvalidateOnAnimation(ScrollableImageView.this);
+            return true;
+        }
+
+        @Override
+        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX,
+                float velocityY) {
+            overScroller.forceFinished(true);
+            overScroller.fling(positionX, positionY, (int) -velocityX,
+                    (int) -velocityY, 0, getMaxHorizontal(), 0,
+                    getMaxVertical());
+            ViewCompat.postInvalidateOnAnimation(ScrollableImageView.this);
+            return true;
+        }
+
+        @Override
+        public boolean onScroll(MotionEvent e1, MotionEvent e2,
+                float distanceX, float distanceY) {
+            overScroller.forceFinished(true);
+            // normalize scrolling distances to not overscroll the image
+            int dx = (int) distanceX;
+            int dy = (int) distanceY;
+            int newPositionX = positionX + dx;
+            int newPositionY = positionY + dy;
+            if (newPositionX < 0) {
+                dx -= newPositionX;
+            } else if (newPositionX > getMaxHorizontal()) {
+                dx -= (newPositionX - getMaxHorizontal());
+            }
+            if (newPositionY < 0) {
+                dy -= newPositionY;
+            } else if (newPositionY > getMaxVertical()) {
+                dy -= (newPositionY - getMaxVertical());
+            }
+            overScroller.startScroll(positionX, positionY, dx, dy, 0);
+            ViewCompat.postInvalidateOnAnimation(ScrollableImageView.this);
+            return true;
+        }
+    };
 
 }
