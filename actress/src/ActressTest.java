23d31bd6e93c1040fbbbc0c0e5972b39345494ab
Prat <banqfirn@gmail.com>
2015-07-04 21:10:54 -0400

    Add Test stub

---
+++ actress/src/ActressTest.java
@@ -0,0 +1,21 @@
+import junit.framework.TestCase;
+import org.junit.After;
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by pt2121 on 7/4/15.
+ */
+public class ActressTest extends TestCase {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+}
