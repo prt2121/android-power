5fa180a32a2e62f553c16d20d4018e48f3fbe74a
Prat <banqfirn@gmail.com>
2015-08-22 09:29:54 -0400

    Move secret

--- FSociety/app/src/main/java/com/prt2121/fsociety/MainActivity.java
+++ FSociety/app/src/main/java/com/prt2121/fsociety/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        CameraActivity.startCameraActivityForResult(this, "", 777);
+        CameraActivity.startCameraActivityForResult(this, getString(R.string.key), 777);
     }
 
     @Override
